5bd0be20e605c4f45780206933f95e45d6279da8
Elu620758 <elumalai620758@gmail.com>
2023-07-01 14:18:03 +0530

    first commit

---
+++ src/Products.js
@@ -0,0 +1,78 @@
+const ProductData = [
+    {
+        id: 1,
+        title: 'OnePlus Note 11',
+        price: '699',
+        rating: 4,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/41eSQvTbsaL._SX300_SY300_QL70_FMwebp_.jpg'
+    },
+    {
+        id: 2,
+        title: 'Motorola One',
+        price: '499',
+        rating: 4,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/61i760IyvfL._SY741_.jpg'
+    },
+    {
+        id: 3,
+        title: 'Samsung Galaxy S23',
+        price: '249',
+        rating: 3,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/61imYpK33qL._SL1500_.jpg',
+    },
+    {
+        id: 4,
+        title: 'Nothing Phone 2',
+        price: '299',
+        rating: 5,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/71mrFTSyxwL._SX569_.jpg'
+    },
+    {
+        id: 5,
+        title: 'Apple Iphone 14 Pro',
+        price: '349',
+        rating: 4,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/31irWzsdLsL._SY445_SX342_QL70_FMwebp_.jpg'
+    },
+    {
+        id: 6,
+        title: 'Vivo Y30',
+        price: '154',
+        rating: 3,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/417wM7M8fdL._SX300_SY300_QL70_FMwebp_.jpg'
+    },
+    {
+        id: 7,
+        title: 'Lava Z10',
+        price: '175',
+        rating: 3,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/51RtjBcZqWL._SX679_.jpg',
+    },
+    {
+        id: 8,
+        title: 'Motorola G9',
+        price: '265',
+        rating: 4,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/41wrR7XECsL._SX300_SY300_QL70_FMwebp_.jpg'
+    },
+    {
+        id: 9,
+        title: 'Motorola g6',
+        price: '215',
+        rating: 4,
+        cart: false,
+        image: 'https://m.media-amazon.com/images/I/61i760IyvfL._SY741_.jpg'
+    },
+
+
+];
+
+export default ProductData;
